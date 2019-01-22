ca30455b4d524b60dce73c29c672376b33787df0
Frderik Kriewitz <frederik@kriewitz.eu>
2019-01-22 16:56:00 +0000

    initial commit

---
+++ PDFOCGtool/PDFOCGtool.cs
@@ -0,0 +1,87 @@
+﻿using iText.Kernel.Pdf;
+using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Reflection;
+
+namespace PDFOCGtool
+{
+    class PDFOCGtool
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+
+            app.Name = "pdfocgtool";
+            app.Description = "PDF OCG manipulation tool";
+            app.ExtendedHelpText = "This is a simple tool to modify Optional Content Group (OCG) realted options of PDF files.";
+
+            app.HelpOption("-?|-h|--help");
+
+            app.VersionOption("-v|--version", () => {
+                return string.Format("Version {0}", Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion);
+            });
+
+            var input = app.Option("-i|--input <file>", "Path to the input file", CommandOptionType.SingleValue);
+            var output = app.Option("-o|--output <file>", "Path to the output file", CommandOptionType.SingleValue);
+
+            var onOptions = app.Option("--on <layer>", "Set visibility to on for layer", CommandOptionType.MultipleValue);
+            var offOptions = app.Option("--off <layer>", "Set visibility to off for layer", CommandOptionType.MultipleValue);
+
+            app.OnExecute(() =>
+            {
+                if (!input.HasValue())
+                {
+                    Console.Error.WriteLine("Input file not specified");
+                    app.ShowHint();
+                    return 1;
+                }
+
+                if (!output.HasValue())
+                {
+                    Console.Error.WriteLine("Output file not specified");
+                    app.ShowHint();
+                    return 1;
+                }
+
+                var src = input.Value();
+                var dest = output.Value();
+                PdfDocument pdf = new PdfDocument(new PdfReader(src), new PdfWriter(dest));
+
+                var catalog = pdf.GetCatalog();
+                var oCProperties = catalog.GetOCProperties(true);
+                var layers = oCProperties.GetLayers();
+                foreach (var layer in layers)
+                {
+                    var name = layer.GetPdfObject().GetAsString(PdfName.Name)?.ToUnicodeString();
+                    if (onOptions.Values.Contains(name))
+                    {
+                        layer.SetOn(true);
+                    }
+                    else if (offOptions.Values.Contains(name))
+                    {
+                        layer.SetOn(false);
+                    }
+                    Console.WriteLine(name);
+                }
+
+                pdf.Close();
+
+                return 0;
+            });
+
+
+            try
+            {
+                app.Execute(args);
+            }
+            catch (CommandParsingException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while executing the application: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# PDFOCGtool
+
+PDFOCGtool is a quick and dirty CLI tool to enable/disable the default visibility of Optional Content Groups (layers) in PDF files.
+
+It uses .NET Core and the [itext7-dotnet](https://github.com/itext/itext7-dotnet) library to archive this.
+
+## Usage
+
+```
+Usage: PDFOCGtool [options]
+
+Options:
+  -?|-h|--help        Show help information
+  -v|--version        Show version information
+  -i|--input <file>   Path to the input file
+  -o|--output <file>  Path to the output file
+  --on <layer>        Set visibility to on for layer
+  --off <layer>       Set visibility to off for layer
+```
+
+## Building from source
+
+### Linux
+```bash
+apt install dotnet-sdk-2.2
+git clone https://github.com/freddy36/pdfogctool.git
+cd pdfogctool
+dotnet publish PDFOCGtool -f netcoreapp2.2 --self-contained -r linux-x64 -c Debug
+
+./PDFOCGtool/bin/Debug/netcoreapp2.2/linux-x64/pdfocgtool --help
+```
+
+### Windows
+Use Visual Studio with .NET Core SDK 2.2 to open and build the project.
+
+## License 
+AGPL due to itext7
